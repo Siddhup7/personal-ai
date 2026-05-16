f672d9f95de9d006eebf8066f84645e85a89b597
siddhup7 <2400031703@kluniversity.in>
2026-05-16 11:54:04 +0530

    Connected railway database

--- database.py
+++ database.py
@@ -1,37 +1,12 @@
 import mysql.connector
 import os
 
-# =========================
-# MYSQL CONNECTION
-# =========================
-
 db = mysql.connector.connect(
-    host=os.getenv("MYSQL_HOST", "localhost"),
-    user=os.getenv("MYSQL_USER", "root"),
-    password=os.getenv("MYSQL_PASSWORD", "Siddhu*2006"),
-    database=os.getenv("MYSQL_DATABASE", "personal_ai")
+    host=os.getenv("MYSQL_HOST"),
+    port=os.getenv("MYSQL_PORT"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    database=os.getenv("MYSQL_DATABASE")
 )
 
 cursor = db.cursor()
-
-# =========================
-# CREATE MEMORY TABLE
-# =========================
-
-cursor.execute("""
-
-CREATE TABLE IF NOT EXISTS memory (
-
-    id INT AUTO_INCREMENT PRIMARY KEY,
-
-    user_message TEXT,
-
-    ai_response TEXT,
-
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-
-)
-
-""")
-
-db.commit()
